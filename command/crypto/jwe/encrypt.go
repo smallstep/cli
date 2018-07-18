4572b9ad510b506c712dd2c14e667e19e7b1d7b0
Mariano Cano <mariano@smallstep.com>
2018-07-18 15:08:11 -0700

    Add markdown formatting to step crypt jwe encrypt

--- command/crypto/jwe/encrypt.go
+++ command/crypto/jwe/encrypt.go
@@ -16,58 +16,75 @@ func encryptCommand() cli.Command {
 		Name:   "encrypt",
 		Action: cli.ActionFunc(encryptAction),
 		Usage:  "encrypt a payload using JSON Web Encryption (JWE)",
-		UsageText: `step crypto jwe encrypt [--alg KEY_ENC_ALGORITHM] [--enc CONTENT_ENC_ALGORITHM]
-  		[--key JWK] [--jwks JWKS] [--kid KID]`,
-		Description: `The 'step crypto jwe encrypt' command encrypts a payload using JSON Web
-Encryption (JWE). By default, the payload to encrypt is read from STDIN and
-the JWE data structure will be written to STDOUT.`,
+		UsageText: `**step crypto jwe encrypt**
+		[**--alg**=<key-enc-algorithm>] [**--enc**=<content-enc-algorithm>]
+  		[**--key**=<jwk>] [**--jwks**=<jwks>] [**--kid**=<kid>]`,
+		Description: `**step crypto jwe encrypt** encrypts a payload using JSON Web Encryption
+(JWE). By default, the payload to encrypt is read from STDIN and the JWE data
+structure will be written to STDOUT.`,
 		Flags: []cli.Flag{
 			cli.StringFlag{
 				Name: "alg, algorithm",
 				Usage: `The cryptographic algorithm used to encrypt or determine the value of the
 content encryption key (CEK). Algorithms are case-sensitive strings defined in
 RFC7518. The selected algorithm must be compatible with the key type. This
-flag is optional. If not specified, the 'alg' member of the JWK is used. If
-the JWK has no "alg" member then a default is selected depending on the JWK
-key type. If the JWK has an "alg" member and the "alg" flag is passed the two
-options must match unless the '--subtle' flag is also passed.
+flag is optional. If not specified, the **"alg"** member of the JWK is used. If
+the JWK has no **"alg"** member then a default is selected depending on the JWK
+key type. If the JWK has an **"alg"** member and the **--alg** flag is passed the two
+options must match unless the **--subtle** flag is also passed.
 
-  KEY_ENC_ALGORITHM is a case-sensitive string and must be one of:
+: <key-enc-algorithm> is a case-sensitive string and must be one of:
 
-    RSA1_5
-     RSAES-PKCS1-v1_5
-    RSA-OAEP
-     RSAES OAEP using default parameters
-    RSA-OAEP-256
-     RSAES OAEP using SHA-256 and MGF1 with SHA-256
-    A128KW
-     AES Key Wrap with default initial value using 128-bit key
-    A192KW
-     AES Key Wrap with default initial value using 192-bit key
-    A256KW
-     AES Key Wrap with default initial value using 256-bit key
-    dir
-     Direct use of a shared symmetric key as the content encryption key (CEK)
-    ECDH-ES (default for EC keys)
-     Elliptic Curve Diffie-Hellman Ephemeral Static key agreement
-    ECDH-ES+A128KW
-     ECDH-ES using Concat KDF and CEK wrapped with "A128KW"
-    ECDH-ES+A192KW
-     ECDH-ES using Concat KDF and CEK wrapped with "A192KW"
-    ECDH-ES+A256KW
-     ECDH-ES using Concat KDF and CEK wrapped with "A256KW"
-    A128GCMKW
-     Key wrappiung with AES GCM using 128-bit key
-    A192GCMKW
-     Key wrappiung with AES GCM using 192-bit key
-    A256GCMKW
-     Key wrappiung with AES GCM using 256-bit key
-    PBES2-HS256+A128KW
-     PBES2 with HMAC SHA-256 and "A128KW" wrapping
-    PBES2-HS384+A192KW
-     PBES2 with HMAC SHA-256 and "A192KW" wrapping
-    PBES2-HS512+A256KW
-     PBES2 with HMAC SHA-256 and "A256KW" wrapping`,
+    **RSA1_5**
+    :  RSAES-PKCS1-v1_5
+
+    **RSA-OAEP**
+    :  RSAES OAEP using default parameters
+
+    **RSA-OAEP-256** (default for RSA keys)
+    :  RSAES OAEP using SHA-256 and MGF1 with SHA-256
+
+    **A128KW**
+    :  AES Key Wrap with default initial value using 128-bit key
+
+    **A192KW**
+    :  AES Key Wrap with default initial value using 192-bit key
+
+    **A256KW**
+    :  AES Key Wrap with default initial value using 256-bit key
+
+    **dir**
+    :  Direct use of a shared symmetric key as the content encryption key (CEK)
+
+    **ECDH-ES** (default for EC keys)
+    :  Elliptic Curve Diffie-Hellman Ephemeral Static key agreement
+
+    **ECDH-ES+A128KW**
+    :  ECDH-ES using Concat KDF and CEK wrapped with "A128KW
+
+    **ECDH-ES+A192KW**
+    :  ECDH-ES using Concat KDF and CEK wrapped with "A192KW
+
+    **ECDH-ES+A256KW**
+    :  ECDH-ES using Concat KDF and CEK wrapped with "A256KW
+
+    **A128GCMKW**
+    :  Key wrappiung with AES GCM using 128-bit key
+
+    **A192GCMKW**
+    :  Key wrappiung with AES GCM using 192-bit key
+
+    **A256GCMKW** (default for oct keys)
+    :  Key wrappiung with AES GCM using 256-bit key
+
+    **PBES2-HS256+A128KW**
+    :  PBES2 with HMAC SHA-256 and "A128KW" wrapping
+
+    **PBES2-HS384+A192KW**
+    :  PBES2 with HMAC SHA-256 and "A192KW" wrapping
+
+    **PBES2-HS512+A256KW**
+    :  PBES2 with HMAC SHA-256 and "A256KW" wrapping`,
 			},
 			cli.StringFlag{
 				Name:  "enc, encryption-algorithm",
@@ -76,38 +93,43 @@ options must match unless the '--subtle' flag is also passed.
 encryption on the plaintext payload (the content) to produce ciphertext and
 the authentication tag.
 
-  CONTENT_ENC_ALGORITHM is a case-sensitive string and must be one of:
+: <content-enc-algorithm> is a case-sensitive string and must be one of:
 
-    A128CBC-HS256
-     AES_128_CBC_HMAC_SHA_256 authenticated encryption algorithm
-    A192CBC-HS384
-     AES_192_CBC_HMAC_SHA_384 authenticated encryption algorithm
-    A256CBC-HS512
-     AES_256_CBC_HMAC_SHA_512 authenticated encryption algorithm
-    A128GCM
-     AES GCM using 128-bit key
-    A192GCM
-     AES GCM using 192-bit key
-    A256GCM (default)
-     AES GCM using 256-bit key`,
+    **A128CBC-HS256**
+    :  AES_128_CBC_HMAC_SHA_256 authenticated encryption algorithm
+
+    **A192CBC-HS384**
+    :  AES_192_CBC_HMAC_SHA_384 authenticated encryption algorithm
+
+    **A256CBC-HS512**
+    :  AES_256_CBC_HMAC_SHA_512 authenticated encryption algorithm
+
+    **A128GCM**
+    :  AES GCM using 128-bit key
+
+    **A192GCM**
+    :  AES GCM using 192-bit key
+
+    **A256GCM** (default)
+    :  AES GCM using 256-bit key`,
 			},
 			cli.StringFlag{
 				Name: "key",
-				Usage: `The JWE recipient's public key. The KEY argument should be the name of a
+				Usage: `The JWE recipient's public key. The <key> argument should be the name of a
 file. JWEs can be encrypted for a recipient using a public JWK or a PEM
 encoded public key.`,
 			},
 			cli.StringFlag{
 				Name: "jwks",
-				Usage: `The JWK Set containing the recipient's public key. The JWKS argument should
-be the name of a file. The file contents should be a JWK Set. The '--jwks'
-flag requires the use of the '--kid' flag to specify which key to use.`,
+				Usage: `The JWK Set containing the recipient's public key. The <jwks> argument should
+be the name of a file. The file contents should be a JWK Set. The **--jwks**
+flag requires the use of the **--kid** flag to specify which key to use.`,
 			},
 			cli.StringFlag{
 				Name: "kid",
-				Usage: `The ID of the recipient's public key. KID is a case-sensitive string. When
-used with '--key' the KID value must match the "kid" member of the JWK. When
-used with '--jwks' (a JWK Set) the KID value must match the "kid" member of
+				Usage: `The ID of the recipient's public key. <kid> is a case-sensitive string. When
+used with **--key** the <kid> value must match the **"kid"** member of the JWK. When
+used with **--jwks** (a JWK Set) the <kid> value must match the **"kid"** member of
 one of the JWKs in the JWK Set.`,
 			},
 			cli.StringFlag{
@@ -132,6 +154,10 @@ applications that use JWE.`,
 }
 
 func encryptAction(ctx *cli.Context) error {
+	if err := errs.NumberOfArguments(ctx, 0); err != nil {
+		return err
+	}
+
 	data, err := utils.ReadAll(os.Stdin)
 	if err != nil {
 		return err
@@ -288,7 +314,7 @@ func getRecipientAlg(ctx *cli.Context, alg string) (jose.KeyAlgorithm, error) {
 		return jose.A192KW, nil
 	case "A256KW":
 		return jose.A256KW, nil
-	case "DIRECT":
+	case "dir":
 		return jose.DIRECT, nil
 	case "ECDH-ES":
 		return jose.ECDH_ES, nil
