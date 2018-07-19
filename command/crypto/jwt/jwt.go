6f2abcc5d5f1738575a35179274132283b84a58a
Mariano Cano <mariano@smallstep.com>
2018-07-18 18:56:45 -0700

    Use markdown on step crypto jwt docs.

--- command/crypto/jwt/jwt.go
+++ command/crypto/jwt/jwt.go
@@ -9,7 +9,7 @@ func Command() cli.Command {
 	return cli.Command{
 		Name:      "jwt",
 		Usage:     "sign and verify data using JSON Web Tokens (JWT)",
-		UsageText: "step crypto jwt SUBCOMMAND [SUBCOMMAND_ARGUMENTS] [GLOBAL_FLAGS] [SUBCOMMAND_FLAGS]",
+		UsageText: "step crypto jwt <subcommand> [arguments] [global-flags] [subcommand-flags]",
 		Description: `A JSON Web Token or JWT (pronounced "jot") is a compact data structure used
 to represent some JSON encoded "claims" that are passed as the payload of a
 JWS or JWE structure, enabling the claims to be digitally signed and/or
@@ -18,11 +18,11 @@ object. JWTs are represented using a compact format that's URL safe and can be
 used in space-constrained environments. JWTs can be passed in HTTP
 Authorization headers and as URI query parameters.
 
-  A "claim" is a piece of information asserted about a subject, represented as
-a key/value pair. Logically a verified JWT can be interpreted as "ISSUER says
-to AUDIENCE that SUBJECT's CLAIM_NAME is CLAIM_VALUE" for each claim.
+A "claim" is a piece of information asserted about a subject, represented as a
+key/value pair. Logically a verified JWT can be interpreted as "<issuer> says to
+<audience> that <subject>'s <claim-name> is <claim-value>" for each claim.
 
-  A JWT signed using JWS has three parts:
+A JWT signed using JWS has three parts:
 
     1. A base64 encoded JSON object representing the JOSE (JSON Object Signing
        and Encryption) header that describes the cryptographic operations
