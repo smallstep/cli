84601b26ef5d9c35657268d31f689d9400473e9e
Mariano Cano <mariano@smallstep.com>
2018-07-18 18:46:39 -0700

    Remove extra spaces used in description

--- command/crypto/jwk/jwk.go
+++ command/crypto/jwk/jwk.go
@@ -12,7 +12,7 @@ func Command() cli.Command {
 (JSON Web Keys) as defined in RFC7517. It also includes command line utilities
 for managing Key Sets and working with encrypted keys.
 
-  A JWK is a JSON data structure that represents a cryptographic key. The
+A JWK is a JSON data structure that represents a cryptographic key. The
 members of this data structure represent properties of the key, including its
 value. A JWK Set is a simple data structure for representing a set of JWKs. A
 JWK Set is a JSON object with a "keys" member whose value is an array of JWKs.
@@ -21,7 +21,7 @@ JSON Web Algorithms (JWA) specification in RFC7518. This tool also supports
 extensions defined in standards track RFC8037 defining curve and algorithm
 identifiers for Edwards-curve Digial Signatures.
 
-  JWKs and JWK Sets are used in the JSON Web Signature (JWS; RFC7515) and JSON
+JWKs and JWK Sets are used in the JSON Web Signature (JWS; RFC7515) and JSON
 Web Encryption (JWE; RFC7516) specifications for signing and encrypting JSON
 data, respectively.`,
 		Subcommands: cli.Commands{
