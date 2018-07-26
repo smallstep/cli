152b30ab0eaabaa8ecce5fa032f7862dd6949fe2
Mariano Cano <mariano@smallstep.com>
2018-07-26 15:37:44 -0700

    Add examples to step crypto otp.

--- command/crypto/otp/generate.go
+++ command/crypto/otp/generate.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"image/png"
+	"strings"
 
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
 	"github.com/smallstep/cli/errs"
 	"github.com/smallstep/cli/utils"
 	"github.com/urfave/cli"
@@ -97,4 +100,32 @@ func generateAction(ctx *cli.Context) error {
 	}
 
 	return nil
+}
+
+func algFromString(ctx *cli.Context, alg string) (otp.Algorithm, error) {
+	switch strings.ToUpper(alg) {
+	case "SHA1":
+		return otp.AlgorithmSHA1, nil
+	case "SHA256":
+		return otp.AlgorithmSHA256, nil
+	case "SHA512":
+		return otp.AlgorithmSHA512, nil
+	default:
+		return 0, errs.InvalidFlagValue(ctx, "alg", alg, "SHA1, SHA256, or SHA512")
+	}
+}
+
+func generate(ctx *cli.Context) (*otp.Key, error) {
+	alg, err := algFromString(ctx, ctx.String("alg"))
+	if err != nil {
+		return nil, err
+	}
+	return totp.Generate(totp.GenerateOpts{
+		Issuer:      ctx.String("issuer"),
+		AccountName: ctx.String("account"),
+		Period:      uint(ctx.Int("period")),
+		SecretSize:  uint(ctx.Int("secret-size")),
+		Digits:      otp.Digits(ctx.Int("length")),
+		Algorithm:   alg,
+	})
 }
--- command/crypto/otp/otp.go
+++ command/crypto/otp/otp.go
@@ -1,52 +1,38 @@
 package otp
 
 import (
-	"strings"
-
-	"github.com/pquerna/otp"
-	"github.com/pquerna/otp/totp"
-	"github.com/smallstep/cli/errs"
 	"github.com/urfave/cli"
 )
 
 // Command returns the cli.Command for jwt and related subcommands.
 func Command() cli.Command {
 	return cli.Command{
-		Name:        "otp",
-		Usage:       "generate and verify one-time passwords",
-		UsageText:   "step crypto otp <subcommand> [arguments] [global-flags] [subcommand-flags]",
-		Description: `Implements TOTP and HOTP one-time passwords (mention RFCs)`,
+		Name:      "otp",
+		Usage:     "generate and verify one-time passwords",
+		UsageText: "step crypto otp <subcommand> [arguments] [global-flags] [subcommand-flags]",
+		Description: `**step crypto otp** command group implements TOTP and HOTP one-time passwords
+(mention RFCs)
+
+## EXAMPLE
+
+Generate a new TOTP token and it's QR Code to scan:
+'''
+$ step crypto otp generate --issuer smallstep.com --account mariano@smallstep.com -qr smallstep.png \> smallstep.totp
+
+$ cat smallstep.totp
+55RU6WTUISKKGEYVNSSI7H6FTJWJ4IPP
+'''
+
+Scan the QR Code using Google Authenticator, Authy or a similar software and
+use it to verify the TOTP token:
+'''
+$ step crypto otp verify --secret smallstep.totp
+Enter Passcode: 614318
+ok
+'''`,
 		Subcommands: cli.Commands{
 			generateCommand(),
 			verifyCommand(),
 		},
 	}
-}
-
-func algFromString(ctx *cli.Context, alg string) (otp.Algorithm, error) {
-	switch strings.ToUpper(alg) {
-	case "SHA1":
-		return otp.AlgorithmSHA1, nil
-	case "SHA256":
-		return otp.AlgorithmSHA256, nil
-	case "SHA512":
-		return otp.AlgorithmSHA512, nil
-	default:
-		return 0, errs.InvalidFlagValue(ctx, "alg", alg, "SHA1, SHA256, or SHA512")
-	}
-}
-
-func generate(ctx *cli.Context) (*otp.Key, error) {
-	alg, err := algFromString(ctx, ctx.String("alg"))
-	if err != nil {
-		return nil, err
-	}
-	return totp.Generate(totp.GenerateOpts{
-		Issuer:      ctx.String("issuer"),
-		AccountName: ctx.String("account"),
-		Period:      uint(ctx.Int("period")),
-		SecretSize:  uint(ctx.Int("secret-size")),
-		Digits:      otp.Digits(ctx.Int("length")),
-		Algorithm:   alg,
-	})
 }
