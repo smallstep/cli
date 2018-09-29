c637430e9f9289a730d4c2bf366bc0aec0979763
Jay Gabriels <jgabriels30@gmail.com>
2018-09-28 19:52:55 -0700

    add logging for invalid http requests into the oauth server
    Log HTTP requests to OAuth HTTP server #29

--- command/oauth/cmd.go
+++ command/oauth/cmd.go
@@ -558,13 +558,17 @@ func (o *oauth) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	code := q.Get("code")
+	code, state := q.Get("code"), q.Get("state")
+
+	if code == "" || state == "" {
+		fmt.Printf("Invalid request received from: %v%v\n\n", req.RemoteAddr, req.RequestURI)
+	}
+
 	if code == "" {
 		o.badRequest(w, "Failed to authenticate: missing or invalid code")
 		return
 	}
 
-	state := q.Get("state")
 	if state == "" || state != o.state {
 		o.badRequest(w, "Failed to authenticate: missing or invalid state")
 		return
