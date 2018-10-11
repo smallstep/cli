35add5844650b87ca65b3fa9a6aab541cf0cbde8
Mariano Cano <mariano@smallstep.com>
2018-10-11 12:18:25 -0700

    Set EnvVar for all flags.

--- command/command.go
+++ command/command.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"strings"
+
 	"github.com/smallstep/cli/usage"
 	"github.com/urfave/cli"
 )
@@ -13,12 +15,102 @@ func init() {
 	}
 }
 
-// Register adds the given command to the global list of commands
+// Register adds the given command to the global list of commands.
+// It sets recursively the command Flags environment variables.
 func Register(c cli.Command) {
+	setEnvVar(&c)
 	cmds = append(cmds, c)
 }
 
 // Retrieve returns all commands
 func Retrieve() []cli.Command {
 	return cmds
+}
+
+// getEnvVar generates the environment variable for the given flag name.
+func getEnvVar(name string) string {
+	parts := strings.Split(name, ",")
+	name = strings.TrimSpace(parts[0])
+	name = strings.Replace(name, "-", "_", -1)
+	return "STEP_" + strings.ToUpper(name)
+}
+
+// setEnvVar sets the the EnvVar element to each flag recursively.
+func setEnvVar(c *cli.Command) {
+	if c == nil {
+		return
+	}
+	for i, flag := range c.Flags {
+		envVar := getEnvVar(flag.GetName())
+		switch f := flag.(type) {
+		case cli.BoolFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.BoolTFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.DurationFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.Float64Flag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.GenericFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.Int64Flag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.IntFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.IntSliceFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.Int64SliceFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.StringFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.StringSliceFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.Uint64Flag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		case cli.UintFlag:
+			if f.EnvVar == "" {
+				f.EnvVar = envVar
+				c.Flags[i] = f
+			}
+		}
+	}
+	for _, cmd := range c.Subcommands {
+		setEnvVar(&cmd)
+	}
 }
