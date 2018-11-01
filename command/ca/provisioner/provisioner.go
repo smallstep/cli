8a741276ffd00cf2de56aa5b50d9280f24a0bc88
Mariano Cano <mariano@smallstep.com>
2018-11-01 11:59:55 -0700

    Add help for step ca provisioner.
    Fixes smallstep/ca-component#100

--- command/ca/provisioner/provisioner.go
+++ command/ca/provisioner/provisioner.go
@@ -6,7 +6,7 @@ import "github.com/urfave/cli"
 func Command() cli.Command {
 	return cli.Command{
 		Name:      "provisioner",
-		Usage:     "create JWKs (JSON Web Keys) and manage JWK Key Sets",
+		Usage:     "create and manage the certificate authority provisioners",
 		UsageText: "step ca provisioner <subcommand> [arguments] [global-flags] [subcommand-flags]",
 		Subcommands: cli.Commands{
 			listCommand(),
@@ -14,7 +14,38 @@ func Command() cli.Command {
 			addCommand(),
 			removeCommand(),
 		},
-		Description: `The **step ca provisioner**
+		Description: `The **step ca provisioner** command group provides facilities for managing the
+certificate authority provisioner.
+
+A provisioner is an entity that controls provisioning credentials, which are
+used to generate provisioning tokens.
+
+Provisioning credentials are simple JWK key pairs using public-key cryptography.
+The public key is used to verify a provisioning token while the private key is
+used to sign the provisioning token.
+
+Provisioning tokens are JWT tokens signed by the JWK private key and are used to
+get a valid TLS certificate from the certificate authority. Each provisioner
+manages a different set of rules that can be used to set the bounds of the
+certificate.
+
+In the certificate authority, a provisioner is configured with a JSON object
+with the following properties:
+
+* **name**: the provisioner name, it will become the JWT issuer and a good
+  practice is to use an email address for this.
+* **type**: the provisioner type, currently only "jwt" is supported.
+* **key**: the JWK public key used to verify the provisioning tokens.
+* **encryptedKey** (optional): the JWE compact serialization of the private key
+  used to sign the provisioning tokens.
+* **claims** (optional): an object with custom options for each provisioner.
+  Options supported are:
+  * **minTLSCertDuration**: minimum duration of a certificate, set to 5m by
+    default.
+  * **maxTLSCertDuration**: maximum duration of a certificate, set to 24h by
+    default.
+  * **defaultTLSCertDuration**: default duration of the certificate, set to 24h
+    by default.
 
 ## EXAMPLES
 
