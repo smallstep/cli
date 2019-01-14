4995e9d20817d21967676b9741d4a2f153ba9790
Mariano Cano <mariano@smallstep.com>
2019-01-14 11:55:09 -0800

    Remove token requirement for step ca roots and federation.

--- command/ca/federation.go
+++ command/ca/federation.go
@@ -1,12 +1,8 @@
 package ca
 
 import (
-	"context"
 	"encoding/pem"
-	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/smallstep/certificates/api"
@@ -14,10 +10,8 @@ import (
 	"github.com/smallstep/cli/command"
 	"github.com/smallstep/cli/crypto/pemutil"
 	"github.com/smallstep/cli/crypto/pki"
-	"github.com/smallstep/cli/crypto/randutil"
 	"github.com/smallstep/cli/errs"
 	"github.com/smallstep/cli/flags"
-	"github.com/smallstep/cli/jose"
 	"github.com/smallstep/cli/ui"
 	"github.com/smallstep/cli/utils"
 	"github.com/urfave/cli"
@@ -38,10 +32,7 @@ func rootsCommand() cli.Command {
 		UsageText: `**step ca roots** <roots-file>
 		[**--token**=<token>] [**--ca-url**=<uri>] [**--root**=<file>]`,
 		Description: `**step ca roots** downloads a certificate bundle with all the root
-certificates. The request to download the bundle requires
-a mTLS connection with the certificate authority, the command creates a 
-temporal certificate using the token and then does a mTLS request to the 
-CA.
+certificates.
 
 ## POSITIONAL ARGUMENTS
 
@@ -50,19 +41,18 @@ CA.
 
 ## EXAMPLES
 
-Download the roots with a token:
+Download the roots with flags set by <step ca bootstrap>:
 '''
-$ step ca roots --token $(step ca token whatever) roots.pem
+$ step ca roots roots.pem
 '''
 
-Download the roots doing the token flow:
+Download the roots with custom flags:
 '''
 $ step ca roots roots.pem \
     --ca-url https://ca.example.com \
 	--root /path/to/root_ca.crt
 '''`,
 		Flags: []cli.Flag{
-			tokenFlag,
 			caURLFlag,
 			rootFlag,
 			flags.Force,
@@ -78,10 +68,7 @@ func federationCommand() cli.Command {
 		UsageText: `**step ca federation** <federation-file>
 		[**--token**=<token>] [**--ca-url**=<uri>] [**--root**=<file>]`,
 		Description: `**step ca federation** downloads a certificate bundle with all the root
-certificates in the federation. The request to download the bundle requires
-a mTLS connection with the certificate authority, the command creates a 
-temporal certificate using the token and then does a mTLS request to the 
-CA.
+certificates in the federation.
 
 ## POSITIONAL ARGUMENTS
 
@@ -90,12 +77,12 @@ CA.
 
 ## EXAMPLES
 
-Download the federated roots with a token:
+Download the federated roots with flags set by <step ca bootstrap>:
 '''
-$ step ca federation --token $(step ca token whatever) federation.pem
+$ step ca federation federation.pem
 '''
 
-Download the federated roots doing the token flow:
+Download the federated roots with custom flags:
 '''
 $ step ca federation federation.pem \
     --ca-url https://ca.example.com \
@@ -103,7 +90,6 @@ $ step ca federation federation.pem \
 '''
 `,
 		Flags: []cli.Flag{
-			tokenFlag,
 			caURLFlag,
 			rootFlag,
 			flags.Force,
@@ -119,78 +105,25 @@ func federationAction(ctx *cli.Context) error {
 	return rootsAndFederationFlow(ctx, federationFlow)
 }
 
-func getMTLSTransport(client *ca.Client, token string) (*http.Transport, error) {
-	// Create a certificate for temporal use
-	req, pk, err := ca.CreateSignRequest(token)
-	if err != nil {
-		return nil, err
-	}
-	sign, err := client.Sign(req)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	return client.Transport(ctx, sign, pk)
-}
-
 func rootsAndFederationFlow(ctx *cli.Context, typ flowType) error {
 	if err := errs.NumberOfArguments(ctx, 1); err != nil {
 		return err
 	}
 
 	caURL := ctx.String("ca-url")
+	if len(caURL) == 0 {
+		return errs.RequiredFlag(ctx, "ca-url")
+	}
+
 	root := ctx.String("root")
-
-	token := ctx.String("token")
-	if len(token) == 0 {
-		s, err := randutil.Alphanumeric(10)
-		if err != nil {
-			return err
-		}
-
-		subject := "step-cli-" + s
-		token, err = newTokenFlow(ctx, subject, caURL, root, "", "", "", "", time.Time{}, time.Time{})
-		if err != nil {
-			return err
+	if len(root) == 0 {
+		root = pki.GetRootCAPath()
+		if _, err := os.Stat(root); err != nil {
+			return errs.RequiredFlag(ctx, "root")
 		}
 	}
 
-	tok, err := jose.ParseSigned(token)
-	if err != nil {
-		return errors.Wrap(err, "error parsing flag '--token'")
-	}
-	var claims tokenClaims
-	if err := tok.UnsafeClaimsWithoutVerification(&claims); err != nil {
-		return errors.Wrap(err, "error parsing flag '--token'")
-	}
-
-	// Prepare client for bootstrap or provisioning tokens
-	var options []ca.ClientOption
-	if len(claims.SHA) > 0 && len(claims.Audience) > 0 && strings.HasPrefix(strings.ToLower(claims.Audience[0]), "http") {
-		caURL = claims.Audience[0]
-		options = append(options, ca.WithRootSHA256(claims.SHA))
-	} else {
-		if len(caURL) == 0 {
-			return errs.RequiredFlag(ctx, "ca-url")
-		}
-		if len(root) == 0 {
-			root = pki.GetRootCAPath()
-			if _, err := os.Stat(root); err != nil {
-				return errs.RequiredFlag(ctx, "root")
-			}
-		}
-		options = append(options, ca.WithRootFile(root))
-	}
-
-	ui.PrintSelected("CA", caURL)
-	client, err := ca.NewClient(caURL, options...)
-	if err != nil {
-		return err
-	}
-
-	tr, err := getMTLSTransport(client, token)
+	client, err := ca.NewClient(caURL, ca.WithRootFile(root))
 	if err != nil {
 		return err
 	}
@@ -198,13 +131,13 @@ func rootsAndFederationFlow(ctx *cli.Context, typ flowType) error {
 	var certs []api.Certificate
 	switch typ {
 	case rootsFlow:
-		roots, err := client.Roots(tr)
+		roots, err := client.Roots()
 		if err != nil {
 			return err
 		}
 		certs = roots.Certificates
 	case federationFlow:
-		federation, err := client.Federation(tr)
+		federation, err := client.Federation()
 		if err != nil {
 			return err
 		}
