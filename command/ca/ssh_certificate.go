913f36c35beb184b9bdb9701f47378163e002996
Mariano Cano <mariano@smallstep.com>
2019-07-25 16:16:42 -0700

    Add first pass on help for step ca ssh-certificate.

--- command/ca/ssh_certificate.go
+++ command/ca/ssh_certificate.go
@@ -18,38 +18,115 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+var (
+	sshPrincipalFlag = cli.StringSliceFlag{
+		Name: "principal,n",
+		Usage: `Add the principals (users or hosts) that the token is authorized to
+		request. The signing request using this token won't be able to add
+		extra names. Use the '--principal' flag multiple times to configure
+		multiple ones. The '--principal' flag and the '--token' flag are
+		mutually exlusive.`,
+	}
+
+	sshHostFlag = cli.BoolFlag{
+		Name:  "host",
+		Usage: `Create a host certificate instead of a user certificate.`,
+	}
+
+	sshSignFlag = cli.BoolFlag{
+		Name:  "sign",
+		Usage: `Sign the public key passed as an argument instead of creating one.`,
+	}
+)
+
 func sshCertificateCommand() cli.Command {
 	return cli.Command{
-		Name:        "ssh-certificate",
-		Action:      command.ActionFunc(sshCertificateAction),
-		Usage:       "sign an ssh certificate",
-		UsageText:   `**step ca ssh-certificate** <key-id> <key-file>`,
-		Description: `**step ca ssh-certificate** command ...`,
+		Name:   "ssh-certificate",
+		Action: command.ActionFunc(sshCertificateAction),
+		Usage:  "sign a SSH certificate using the the SSH CA",
+		UsageText: `**step ca ssh-certificate** <key-id> <key-file>
+		[**--host**] [**--sign]`,
+		Description: `**step ca ssh-certificate** command generates an SSH key pair and creates a
+certificate using [step certificates](https://github.com/smallstep/certificates).
+
+With a certificate clients or servers may trust only the CA key and verify its
+signature on a certificate rather than trusting many user/host keys.
+
+Note that not all the provisioner types will be able to generate user and host
+certificates. Currently JWK provisioners can generate both, but with an OIDC
+provisioner you will only be able to generate user certificates unless you are
+and admin that can generate both. With a cloud identity provisioner you will
+only be able to generate host certificates.
+
+To configure a server to accept user certificates and provide a user certificate
+you need to add the following lines in </etc/ssh/sshd_config>:
+'''
+# The path to the CA public key, it accepts multiple CAs, one per line
+TrustedUserCAKeys /etc/ssh/ca.pub
+
+# Path to the private key and certificate
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostCertificate /etc/ssh/ssh_host_ecdsa_key-cert.pub
+'''
+
+And to configure a client to accept host certificates you need to add the CA in
+<~/.ssh/known_hosts> with the following format:
+'''
+@cert-authority *.example.com ecdsa-sha2-nistp256 AAAAE...=
+'''
+
+Where <*.example.com> is a pattern that matches the hosts and
+<ecdsa-sha2-nistp256 AAAAE...=> should be the contents of the CA public key.
+
+## POSITIONAL ARGUMENTS
+
+<key-id>
+:  The certificate identity. If no principals are passed we will use
+the key-id as a principal, if it has the format abc@def then the principal will
+be abc.
+
+<key-file>
+:  The private key name when generating a new key pair, or the public
+key path when we are just signing it.
+
+## EXAMPLES
+
+Generate a new SSH key pair and user certificate:
+'''
+$ step ca ssh-certificate mariano@work id_ecdsa
+'''
+
+Sign an SSH public key and generate a user certificate:
+'''
+$ step ca ssh-certificate --sign mariano@work id_ecdsa.pub
+'''
+
+Generate a new SSH key pair and host certificate:
+'''
+$ step ca ssh-certificate --host internal.example.com ssh_host_ecdsa_key
+'''
+
+Sign an SSH public key and generate a host certificate:
+'''
+$ step ca ssh-certificate --host --sign \
+	internal.example.com ssh_host_ecdsa_key.pub
+'''
+
+Generate a new key pair, and a certificate with custom principals (user/host names):
+'''
+$ step ca ssh-certificate --principal max --principal mariano --sign \
+	ops@work id_ecdsa
+'''
+
+Sign an SSH public key generating a certificate with given token:
+'''
+$ step ca ssh-certificate --token $TOKEN mariano@work id_ecdsa
+'''`,
 		Flags: []cli.Flag{
 			tokenFlag,
-			// cli.StringFlag{
-			// 	Name: "type",
-			// 	Usage: `The certificate type to issue (<user|host>). With a certificate clients or
-			// 	servers may trust only the CA key and verify its signature on a certificate
-			// 	rather than trusting many user/host keys.`,
-			// 	Value: provisioner.SSHUserCert,
-			// },
-			cli.StringSliceFlag{
-				Name: "principal,n",
-				Usage: `Add the principals (users or hosts) that the token is authorized to
-				request. The signing request using this token won't be able to add
-				extra names. Use the '--principal' flag multiple times to configure
-				multiple ones. The '--principal' flag and the '--token' flag are
-				mutually exlusive.`,
-			},
-			cli.BoolFlag{
-				Name:  "host",
-				Usage: `Create a host certificate instead of a user certificate.`,
-			},
-			cli.BoolFlag{
-				Name:  "sign",
-				Usage: `Sign the public key passed as an argument instead of creating one.`,
-			},
+			sshPrincipalFlag,
+			sshHostFlag,
+			sshSignFlag,
 			notBeforeCertFlag,
 			notAfterCertFlag,
 			provisionerIssuerFlag,
