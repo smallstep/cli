13a8fae2494543145074e124fce232b4d7f1f83a
Mariano Cano <mariano@smallstep.com>
2019-09-20 16:48:01 -0700

    Use sshutil.Agent.
    Move flag variable to ssh.go

--- command/ssh/ssh.go
+++ command/ssh/ssh.go
@@ -33,3 +33,51 @@ $ step ssh certificate --host internal.example.com ssh_host_ecdsa_key
 
 	command.Register(cmd)
 }
+
+var (
+	sshPrincipalFlag = cli.StringSliceFlag{
+		Name: "principal,n",
+		Usage: `Add the principals (users or hosts) that the token is authorized to
+		request. The signing request using this token won't be able to add
+		extra names. Use the '--principal' flag multiple times to configure
+		multiple ones. The '--principal' flag and the '--token' flag are
+		mutually exlusive.`,
+	}
+
+	sshHostFlag = cli.BoolFlag{
+		Name:  "host",
+		Usage: `Create a host certificate instead of a user certificate.`,
+	}
+
+	sshSignFlag = cli.BoolFlag{
+		Name:  "sign",
+		Usage: `Sign the public key passed as an argument instead of creating one.`,
+	}
+
+	sshPasswordFileFlag = cli.StringFlag{
+		Name:  "password-file",
+		Usage: `The path to the <file> containing the password to encrypt the private key.`,
+	}
+
+	sshProvisionerPasswordFlag = cli.StringFlag{
+		Name: "provisioner-password-file",
+		Usage: `The path to the <file> containing the password to decrypt the one-time token
+		generating key.`,
+	}
+
+	sshAddUserFlag = cli.BoolFlag{
+		Name:  "add-user",
+		Usage: `Create a user provisioner certificate used to create a new user.`,
+	}
+
+	sshPrivateKeyFlag = cli.StringFlag{
+		Name: "private-key",
+		Usage: `When signing an existing public key, use this flag to specify the corresponding
+private key so that the pair can be added to an SSH Agent.`,
+	}
+
+	sshConnectFlag = cli.StringFlag{
+		Name:  "connect,c",
+		Usage: "Remote <host:port> to connect to.",
+	}
+)
