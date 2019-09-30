60f3a03e3b2f8493a13fdfba3888a560f683a74d
Mariano Cano <mariano@smallstep.com>
2019-09-30 16:52:33 -0700

    Add `step ssh list` command.

---
+++ command/ssh/list.go
@@ -0,0 +1,68 @@
+package ssh
+
+import (
+	"fmt"
+
+	"github.com/smallstep/cli/errs"
+
+	"github.com/pkg/errors"
+	"github.com/smallstep/cli/command"
+	"github.com/smallstep/cli/crypto/sshutil"
+	"github.com/urfave/cli"
+)
+
+func listCommand() cli.Command {
+	return cli.Command{
+		Name:      "list",
+		Action:    command.ActionFunc(listAction),
+		Usage:     "list public keys known to the ssh agent",
+		UsageText: `**step ssh list** [<subject>]`,
+		Description: `**step ssh list** list public key identities known to the ssh agent.
+
+## POSITIONAL ARGUMENTS
+
+<ubject>
+:  Optional subject or comment to filter keys by.
+
+## EXAMPLES
+
+List all keys known to the agent:
+'''
+$ step ssh list
+'''
+
+List all the keys with the comment joe@work
+'''
+$ step ssh list joe@work
+'''`,
+	}
+}
+
+func listAction(ctx *cli.Context) error {
+	var subject string
+	if err := errs.MinMaxNumberOfArguments(ctx, 0, 1); err != nil {
+		return err
+	}
+
+	if ctx.NArg() > 0 {
+		subject = ctx.Args().First()
+	}
+
+	agent, err := sshutil.DialAgent()
+	if err != nil {
+		return err
+	}
+
+	keys, err := agent.List()
+	if err != nil {
+		return errors.Wrap(err, "error listing identities")
+	}
+
+	for _, k := range keys {
+		if subject == "" || k.Comment == subject {
+			fmt.Println(k.String())
+		}
+	}
+
+	return nil
+}
