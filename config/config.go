b73167dc6e754f70685a2b19a59f8a426d336efb
Mariano Cano <mariano@smallstep.com>
2019-10-11 18:58:32 -0700

    Enforce detection of home directory.

--- config/config.go
+++ config/config.go
@@ -5,11 +5,10 @@ import (
 	"log"
 	"os"
 	"os/user"
-	"path"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"time"
-
-	"github.com/pkg/errors"
 )
 
 // version and buildTime are filled in during build by the Makefile
@@ -23,8 +22,15 @@ var (
 // the default configuration path.
 const StepPathEnv = "STEPPATH"
 
+// HomeEnv defines the name of the environment variable that can overwrite the
+// default home directory.
+const HomeEnv = "HOME"
+
 // stepPath will be populated in init() with the proper STEPPATH.
 var stepPath string
+
+// homePath will be populated in init() with the proper HOME.
+var homePath string
 
 // StepPath returns the path for the step configuration directory, this is
 // defined by the environment variable STEPPATH or if this is not set it will
@@ -33,20 +39,49 @@ func StepPath() string {
 	return stepPath
 }
 
+// Home returns the user home directory using the environment variable HOME or
+// the os/user package.
+func Home() string {
+	return homePath
+}
+
+// StepAbs returns the given path relative to the StepPath if it's not an
+// absolute path or relative to the home directory using the special string
+// "~/".
+//
+// Relative paths like 'certs/root_ca.crt' will be converted to
+// '$STEPPATH/certs/root_ca.crt'. Home relative paths like ~/certs/root_ca.crt
+// will be converted to '$HOME/certs/root_ca.crt'. And absolute paths like
+// '/certs/root_ca.crt' will remain the same.
+func StepAbs(path string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+	// Windows accept both \ and /
+	if slashed := filepath.ToSlash(path); strings.HasPrefix(slashed, "~/") {
+		return filepath.Join(homePath, path[2:])
+	}
+	return filepath.Join(stepPath, path)
+}
+
 func init() {
 	l := log.New(os.Stderr, "", 0)
 
-	// Get step path from environment or user's home directory
-	stepPath = os.Getenv(StepPathEnv)
-	if stepPath == "" {
+	// Get home path from environment or from the user object.
+	homePath = os.Getenv(HomeEnv)
+	if homePath == "" {
 		usr, err := user.Current()
 		if err == nil && usr.HomeDir != "" {
-			stepPath = path.Join(usr.HomeDir, ".step")
-		} else if home := os.Getenv("HOME"); home != "" {
-			stepPath = path.Join(home, ".step")
+			homePath = usr.HomeDir
 		} else {
-			l.Fatalf("Error obtaining home directory, please define environment variable %s.", StepPathEnv)
+			l.Fatalf("Error obtaining home directory, please define environment variable %s.", HomeEnv)
 		}
+	}
+
+	// Get step path from environment or relative to home.
+	stepPath = os.Getenv(StepPathEnv)
+	if stepPath == "" {
+		stepPath = filepath.Join(homePath, ".step")
 	}
 
 	// Check for presence or attempt to create it if necessary.
@@ -59,20 +94,8 @@ func init() {
 		l.Fatalf("File '%s' is not a directory.", stepPath)
 	}
 	// cleanup
-	stepPath = path.Clean(stepPath)
-}
-
-// Home returns the user home directory using the environment variable HOME or
-// the os/user package.
-func Home() (string, error) {
-	if home := os.Getenv("HOME"); home != "" {
-		return home, nil
-	}
-	usr, err := user.Current()
-	if err != nil || usr.HomeDir == "" {
-		return "", errors.New("error obtaining home directory, please define environment variable HOME")
-	}
-	return usr.HomeDir, nil
+	homePath = filepath.Clean(homePath)
+	stepPath = filepath.Clean(stepPath)
 }
 
 // Set updates the Version and ReleaseDate
