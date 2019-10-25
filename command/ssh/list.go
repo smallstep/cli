a2920dbfe148212409565796cc86087f0a7ef96d
Mariano Cano <mariano@smallstep.com>
2019-10-24 20:33:47 -0700

    List key fingerprints by default.

--- command/ssh/list.go
+++ command/ssh/list.go
@@ -18,6 +18,8 @@ func listCommand() cli.Command {
 		UsageText: `**step ssh list** [<subject>]`,
 		Description: `**step ssh list** list public key identities known to the ssh agent.
 
+By default it prints key fingerprints, to list the raw key use the flag **--raw**.
+
 ## POSITIONAL ARGUMENTS
 
 <ubject>
@@ -25,24 +27,40 @@ func listCommand() cli.Command {
 
 ## EXAMPLES
 
-List all keys known to the agent:
+List all key fingerprints known to the agent:
 '''
 $ step ssh list
 '''
 
-List all the keys with the comment joe@work
+List all the key fingerprints with the comment joe@work:
 '''
 $ step ssh list joe@work
+'''
+
+List all keys known to the agent:
+'''
+$ step ssh list --raw
+'''
+
+List all the keys with the comment joe@work:
+'''
+$ step ssh list --raw joe@work
 '''`,
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "raw",
+				Usage: "List public keys instead of fingerprints.",
+			},
+		},
 	}
 }
 
 func listAction(ctx *cli.Context) error {
-	var subject string
 	if err := errs.MinMaxNumberOfArguments(ctx, 0, 1); err != nil {
 		return err
 	}
 
+	var subject string
 	if ctx.NArg() > 0 {
 		subject = ctx.Args().First()
 	}
@@ -57,9 +75,21 @@ func listAction(ctx *cli.Context) error {
 		return errors.Wrap(err, "error listing identities")
 	}
 
-	for _, k := range keys {
-		if subject == "" || k.Comment == subject {
-			fmt.Println(k.String())
+	if ctx.Bool("raw") {
+		for _, k := range keys {
+			if ctx.NArg() == 0 || k.Comment == subject {
+				fmt.Println(k.String())
+			}
+		}
+	} else {
+		for _, k := range keys {
+			if ctx.NArg() == 0 || k.Comment == subject {
+				s, err := sshutil.Fingerprint([]byte(k.String()))
+				if err != nil {
+					return err
+				}
+				fmt.Println(s)
+			}
 		}
 	}
 
