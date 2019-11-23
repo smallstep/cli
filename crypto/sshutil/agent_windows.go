2943985a000f830adcaa4649c5e83d682fea1476
Mariano Cano <mariano@smallstep.com>
2019-11-22 18:52:51 -0800

    Use two backslashes in named pipe.

--- crypto/sshutil/agent_windows.go
+++ crypto/sshutil/agent_windows.go
@@ -2,7 +2,6 @@ package sshutil
 
 import (
 	"context"
-	"time"
 
 	"github.com/Microsoft/go-winio"
 	"github.com/pkg/errors"
@@ -12,8 +11,7 @@ import (
 // dialAgent returns an ssh.Agent client. It uses the SSH_AUTH_SOCK to connect
 // to the agent.
 func dialAgent() (*Agent, error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	conn, err := winio.DialPipeContext(ctx, "\\.\\pipe\\openssh-ssh-agent")
+	conn, err := winio.DialPipeContext(context.Background(), `\\.\\pipe\\openssh-ssh-agent`)
 	if err != nil {
 		return nil, errors.Wrap(err, "error connecting with ssh-agent")
 	}
