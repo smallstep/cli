4bd825c3991eeb360dc9de4d722f8793d3c77c7c
Mariano Cano <mariano@smallstep.com>
2019-11-25 13:06:42 -0800

    Enable VT100 on windows.

--- utils/sysutils/sysutils_windows.go
+++ utils/sysutils/sysutils_windows.go
@@ -1,6 +1,28 @@
 package sysutils
 
-import "syscall"
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"golang.org/x/sys/windows"
+)
+
+func init() {
+	var inMode, outMode uint32
+	if err := windows.GetConsoleMode(windows.Stdin, &inMode); err == nil {
+		inMode |= windows.ENABLE_VIRTUAL_TERMINAL_INPUT
+		if err := windows.SetConsoleMode(windows.Stdin, inMode); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to set console mode: %v", err)
+		}
+	}
+	if err := windows.GetConsoleMode(windows.Stdout, &outMode); err == nil {
+		outMode |= windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+		if err := windows.SetConsoleMode(windows.Stdout, outMode); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to set console mode: %v", err)
+		}
+	}
+}
 
 func flock(fd int, how int) error {
 	return syscall.EWINDOWS
