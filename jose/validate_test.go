4448ddc1765464093565d80ab80667b9cd941c19
David Narayan <david_narayan@homedepot.com>
2020-04-21 21:33:51 -0400

    Add additional tests to improve test coverage.

--- jose/validate_test.go
+++ jose/validate_test.go
@@ -13,11 +13,45 @@ import (
 )
 
 var (
-	certfile = "./testdata/rsa2048.crt"
-	keyfile  = "./testdata/rsa2048.key"
+	badCertFile = "./testdata/bad-rsa.crt"
+	badKeyFile  = "./testdata/bad-rsa.key"
+	invalidCert = "./testdata/invalid.crt"
+	certfile    = "./testdata/rsa2048.crt"
+	keyfile     = "./testdata/rsa2048.key"
 )
 
 func TestValidateX5T(t *testing.T) {
+	var err error
+
+	// test empty file
+	_, err = ValidateX5T("", nil)
+	assert.Error(t, err, "expected error for empty certfile")
+
+	// test invalid cert
+	_, err = ValidateX5T(invalidCert, nil)
+	assert.Error(t, err, "expected error for invalid cert")
+
+	// test invalid key
+	_, err = ValidateX5T(certfile, nil)
+	assert.Error(t, err, "expected error for invalid key")
+
+	// test cert not approved for DigitalSignature
+	{
+		keyBytes, err := ioutil.ReadFile(badKeyFile)
+		if err != nil {
+			t.Errorf("unable to read badKeyFile=%q: %s", badKeyFile, err)
+			return
+		}
+		keyBlock, _ := pem.Decode(keyBytes)
+		key, err := x509.ParsePKCS8PrivateKey(keyBlock.Bytes)
+		if err != nil {
+			t.Errorf("unable to parse badKeyFile=%q: %s", badKeyFile, err)
+			return
+		}
+		_, err = ValidateX5T(badCertFile, key)
+		assert.Error(t, err, "expected error for cert without digital signature")
+	}
+
 	certBytes, err := ioutil.ReadFile(certfile)
 	if err != nil {
 		t.Errorf("unable to read certfile=%q: %s", certfile, err)
