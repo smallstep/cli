30090249e655179f23923399cba3503beb43f61d
Mariano Cano <mariano@smallstep.com>
2020-09-01 18:17:27 -0700

    Add support for templates in `step certificate sign`
    It also adds support for leaf, intermediate-ca and direct profiles.

--- command/certificate/create.go
+++ command/certificate/create.go
@@ -24,6 +24,7 @@ const (
 	profileSelfSigned     = "self-signed"
 	profileIntermediateCA = "intermediate-ca"
 	profileRootCA         = "root-ca"
+	profileDirect         = "direct" // Used only on sign
 
 	// Default durations
 	defaultLeafValidity         = 24 * time.Hour
