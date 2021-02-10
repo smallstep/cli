12be998821276357137f8a5d4f37a72f3ab058ae
Filippo Tessarotto <zoeslam@gmail.com>
2021-02-10 07:57:54 +0100

    Describe defined values for --prompt

--- command/oauth/cmd.go
+++ command/oauth/cmd.go
@@ -182,8 +182,30 @@ $ step oauth --client-id my-client-id --client-secret my-client-secret \
 				Usage: "OAuth scopes",
 			},
 			cli.StringFlag{
-				Name:  "prompt",
-				Usage: "Whether the Authorization Server prompts the End-User for reauthentication and consent",
+				Name: "prompt",
+				Usage: `Whether the Authorization Server prompts the End-User for reauthentication and consent.
+OpenID standard defines the following values, but your provider may support some or none of them:
+
+    **none**
+    :   The Authorization Server MUST NOT display any authentication or consent user interface pages.
+        An error is returned if an End-User is not already authenticated or the Client does not have
+        pre-configured consent for the requested Claims or does not fulfill other conditions for
+        processing the request.
+
+    **login**
+    :   The Authorization Server SHOULD prompt the End-User for reauthentication. If it cannot
+        reauthenticate the End-User, it MUST return an error, typically login_required.
+
+    **consent**
+    :   The Authorization Server SHOULD prompt the End-User for consent before returning information
+        to the Client. If it cannot obtain consent, it MUST return an error, typically consent_required.
+
+    **select_account**
+    :   The Authorization Server SHOULD prompt the End-User to select a user account. This enables an
+        End-User who has multiple accounts at the Authorization Server to select amongst the multiple
+        accounts that they might have current sessions for. If it cannot obtain an account selection
+        choice made by the End-User, it MUST return an error, typically account_selection_required.
+`,
 			},
 			cli.BoolFlag{
 				Name:  "jwt",
