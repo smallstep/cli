fd845d74b044d1644a707485ccc4db0aa9d1b8bd
TommyA56Code <tommyandersonpersonal56@gmail.com>
2021-06-23 14:43:24 -0500

    Templated With No Actions

--- command/certificate/needsRenewal.go
+++ command/certificate/needsRenewal.go
@@ -1,13 +1,27 @@
 package certificate
 
-import "github.com/urfave/cli"
+import (
+	"github.com/urfave/cli"
+)
 
 func needsRenewalCommand() cli.Command {
 	return cli.Command{
 		Name:   "needs-renewal",
-		Action: cli.ActionFunc(verifyAction),
+		Action: cli.ActionFunc(needsRenewalAction),
 		Usage:  `Check if a certificate needs to be renewed`,
 		UsageText: `**step certificate needs-renewal** <crt_file>  [**--expires-in <duration>]`,
 		Description: ``,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "expires-in",
+				Usage: `Check if the certificate expires in given time duration`,
+			},
+		},
 	}
+}
+
+func needsRenewalAction(ctx *cli.Context) error {
+	
+
+	return nil
 }
