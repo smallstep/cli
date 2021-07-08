0c36611a7ada04fed09d15ae401039d2947102fd
TommyA56Code <tommyandersonpersonal56@gmail.com>
2021-07-08 10:29:42 -0500

    Put the flags and the Descriptions back
    --roots and --servername are in with their descriptions from inspect.go. Examples for --roots were copied and adjusted for needs-renewal purposes

--- command/certificate/needsRenewal.go
+++ command/certificate/needsRenewal.go
@@ -2,6 +2,7 @@ package certificate
 
 import (
 	"crypto/x509"
+	"github.com/smallstep/cli/flags"
 	"strconv"
 	"strings"
 	"time"
@@ -16,10 +17,11 @@ const defaultPercentUsedThreshold = 66
 
 func needsRenewalCommand() cli.Command {
 	return cli.Command{
-		Name:      "needs-renewal",
-		Action:    cli.ActionFunc(needsRenewalAction),
-		Usage:     `Check if a certificate needs to be renewed`,
-		UsageText: `**step certificate needs-renewal** <cert_file or host_name> [**--expires-in**=<duration>]`,
+		Name:   "needs-renewal",
+		Action: cli.ActionFunc(needsRenewalAction),
+		Usage:  `Check if a certificate needs to be renewed`,
+		UsageText: `**step certificate needs-renewal** <cert_file or host_name> [**--expires-in**=<duration>] [**--roots**=<root-bundle>]
+[**--servername**=<servername>]`,
 		Description: `**step certificate needs-renewal** returns '0' if the certificate needs to be renewed based on it's remaining lifetime.
 		Returns '1'  the certificate is within it's validity lifetime bounds and does not need to be renewed.
 		Returns '255' for any other error. By default, a certificate "needs renewal" when it has passed 66% of it's allotted lifetime.
@@ -37,28 +39,60 @@ This command returns '0' if the certificate needs renewal, '1' if the certificat
 ## EXAMPLES
 
 Check certificate for renewal using custom directory:
+
 '''
 $ step certificate needs-renewal ./certificate.crt
 '''
 
 Check certificate for renewal using a hostname:
+
+'''
 $ step certificate needs-renewal https://smallstep.com
 '''
 
 Check if certificate will expire within a given time:
+
+'''
 $ step certificate needs-renewal ./certificate.crt --expires-in 1h15m
 '''
 
 Check if certificate from hostname will expire within a given time:
+
+'''
 $ step certificate needs-renewal https://smallstep.com --expires-in 1h15m
 '''
 
 Check if certificate has passed 75 percent of it's lifetime:
+
+'''
 $ step certificate needs-renewal ./certificate.crt --expires-in 75%
 '''
 
 Check if certificate from a hostname has passed 75 percent of it's lifetime:
+
+'''
 $ step certificate needs-renewal https://smallstep.com --expires-in 75%
+'''
+
+Check a remote certificate using a custom root certificate:
+
+'''
+$ step certificate needs-renewal https://smallstep.com --roots ./root-ca.crt
+'''
+
+Check a remote certificate using a custom list of root certificates:
+
+'''
+$ step certificate needs-renewal https://smallstep.com \
+--roots "./root-ca.crt,./root-ca2.crt,/root-ca3.crt"
+'''
+
+Check a remote certificate using a custom directory of root certificates:
+
+'''
+$ step certificate needs-renewal https://smallstep.com \
+--roots "./path/to/root/certificates/"
+'''
 `,
 		Flags: []cli.Flag{
 			cli.StringFlag{
@@ -69,6 +103,23 @@ character. If using <duration>, the input must be a sequence of decimal numbers,
 each with optional fraction and a unit suffix, such as "300ms", "-1.5h" or "2h45m".
 Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".`,
 			},
+			cli.StringFlag{
+				Name: "roots",
+				Usage: `Root certificate(s) that will be used to verify the
+authenticity of the remote server.
+
+: <roots> is a case-sensitive string and may be one of:
+
+    **file**
+	:  Relative or full path to a file. All certificates in the file will be used for path validation.
+
+    **list of files**
+	:  Comma-separated list of relative or full file paths. Every PEM encoded certificate from each file will be used for path validation.
+
+    **directory**
+	:  Relative or full path to a directory. Every PEM encoded certificate from each file in the directory will be used for path validation.`,
+			},
+			flags.ServerName,
 		},
 	}
 }
@@ -79,16 +130,18 @@ func needsRenewalAction(ctx *cli.Context) error {
 	}
 
 	var (
-		err       error
-		crtFile   = ctx.Args().Get(0)
-		expiresIn = ctx.String("expires-in")
+		err        error
+		crtFile    = ctx.Args().Get(0)
+		expiresIn  = ctx.String("expires-in")
+		roots      = ctx.String("roots")
+		serverName = ctx.String("servername")
 	)
 
 	var certs []*x509.Certificate
 	if addr, isURL, err := trimURL(crtFile); err != nil {
 		return errs.NewExitError(err, 255)
 	} else if isURL {
-		certs, err = getPeerCertificates(addr, "", "", false)
+		certs, err = getPeerCertificates(addr, serverName, roots, false)
 		if err != nil {
 			return errs.NewExitError(err, 255)
 		}
