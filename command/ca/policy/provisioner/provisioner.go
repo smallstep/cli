55f9057edb5a9d89958dd5bef0a21a6982d1f3ca
Herman Slatman <hermanslatman@hotmail.com>
2022-06-01 13:43:02 +0200

    Add note about provisioner/acme policies availability in CM only

--- command/ca/policy/provisioner/provisioner.go
+++ command/ca/policy/provisioner/provisioner.go
@@ -15,10 +15,14 @@ import (
 func Command(ctx context.Context) cli.Command {
 	ctx = policycontext.WithProvisionerPolicyLevel(ctx)
 	return cli.Command{
-		Name:        "provisioner",
-		Usage:       "manage certificate issuance policies for provisioners",
-		UsageText:   "**step ca policy provisioner** <subcommand> [arguments] [global-flags] [subcommand-flags]",
-		Description: `**step ca policy provisioner** command group provides facilities for managing certificate issuance policies for provisioners.`,
+		Name:      "provisioner",
+		Usage:     "manage certificate issuance policies for provisioners",
+		UsageText: "**step ca policy provisioner** <subcommand> [arguments] [global-flags] [subcommand-flags]",
+		Description: `**step ca policy provisioner** command group provides facilities for managing certificate issuance policies for provisioners.
+
+Please note that certificate issuance policies on the provisioner level are currently only supported in Certificate Manager: https://u.step.sm/cm.		
+
+`,
 		Subcommands: cli.Commands{
 			actions.ViewCommand(ctx),
 			actions.RemoveCommand(ctx),
