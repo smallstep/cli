7d03985cd2f5dafcb7236d4437f3a957732a59c1
max furman <mx.furman@gmail.com>
2022-10-26 23:28:15 -0700

    [action] cosign over docker image digest

--- make/docker.mk
+++ make/docker.mk
@@ -55,7 +55,6 @@ define DOCKER_BUILDX
 	# $(1) -- Image Tag
 	# $(2) -- Push (empty is no push | --push will push to dockerhub)
 	docker buildx build . --progress plain -t $(DOCKER_IMAGE_NAME):$(1) -f docker/Dockerfile --platform="$(DOCKER_PLATFORMS)" $(2)
-	cosign sign -r $(DOCKER_IMAGE_NAME):$(1)
 endef
 
 # For non-master builds don't build the docker containers.
