26e2b891e2b920342103522e2182b2c0fc7c4f2f
Andrew Reed <w.andrew.reed@gmail.com>
2023-02-16 10:16:18 -0600

    Add step api token create command (#842)

---
+++ command/api/token/token.go
@@ -0,0 +1,29 @@
+package token
+
+import (
+	"github.com/urfave/cli"
+)
+
+// Command returns the token subcommand.
+func Command() cli.Command {
+	return cli.Command{
+		Name:      "token",
+		Usage:     "create tokens for connecting to the Smallstep API",
+		UsageText: "step api token <subcommand> [arguments] [global-flags] [subcommand-flags]",
+		Subcommands: cli.Commands{
+			createCommand(),
+		},
+		Description: `**step api token** command group provides commands for creating the
+tokens required to connect to the Smallstep API.
+`,
+	}
+}
+
+// common flags
+var (
+	apiURLFlag = cli.StringFlag{
+		Name:  "api-url",
+		Usage: "URL where the Smallstep API can be found",
+		Value: "https://gateway.smallstep.com",
+	}
+)
