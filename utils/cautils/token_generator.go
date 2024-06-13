615f8c89420783f2dbe990b8db544d8f8887609d
Mariano Cano <mariano.cano@gmail.com>
2024-06-12 17:19:16 -0700

    Fix linter warnings

--- utils/cautils/token_generator.go
+++ utils/cautils/token_generator.go
@@ -126,14 +126,12 @@ func generateOIDCToken(ctx *cli.Context, p *provisioner.OIDC) (string, error) {
 		"--client-id", p.ClientID, "--client-secret", p.ClientSecret}
 	if len(p.Scopes) != 0 {
 		for _, keyval := range p.Scopes {
-			args = append(args, "--scope")
-			args = append(args, keyval)
+			args = append(args, "--scope", keyval)
 		}
 	}
 	if len(p.AuthParams) != 0 {
 		for _, keyval := range p.AuthParams {
-			args = append(args, "--auth-param")
-			args = append(args, keyval)
+			args = append(args, "--auth-param", keyval)
 		}
 	}
 	if ctx.Bool("console") {
