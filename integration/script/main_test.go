825b1617e3b34ac7ea8b410c1259bf939cb0c0b1
Herman Slatman <hermanslatman@hotmail.com>
2025-05-27 13:51:20 +0200

    Replace several old integration style tests with `testscript` tests

---
+++ integration/script/main_test.go
@@ -0,0 +1,19 @@
+package script
+
+import (
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestVersionCommand(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Files: []string{"testdata/version.txtar"},
+	})
+}
+
+func TestBogusCommandFails(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Files: []string{"testdata/bogus.txtar"},
+	})
+}
---
+++ integration/script/shared_test.go
@@ -0,0 +1,16 @@
+package script
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+
+	"github.com/smallstep/cli/internal/cmd"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"step": cmd.Run,
+	}))
+}
