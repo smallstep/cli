298ff27d8c204e0ebca62817eb72766da2931091
Herman Slatman <hermanslatman@hotmail.com>
2025-06-12 15:22:55 +0200

    Refactor `step crypto otp` tests into using `testscript`

--- integration/script/crypto_test.go
+++ integration/script/crypto_test.go
@@ -14,6 +14,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
 	"github.com/rogpeppe/go-internal/testscript"
 	"github.com/stretchr/testify/require"
 
@@ -74,38 +76,74 @@ func TestCryptoKeyPair(t *testing.T) {
 	})
 }
 
+func TestCryptoOTP(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Files: []string{"testdata/crypto/otp.txtar"},
+		Setup: func(e *testscript.Env) error {
+			secret := "UPCTJYT7MUR4RWOUJ3TGTUB43IYCBJ76"
+			err := os.WriteFile(filepath.Join(e.Cd, "secret.txt"), []byte(secret), 0600)
+			require.NoError(t, err)
+			code, err := totp.GenerateCode(secret, time.Now())
+			require.NoError(t, err)
+			err = os.WriteFile(filepath.Join(e.Cd, "code.txt"), []byte(code), 0600)
+			require.NoError(t, err)
+			err = os.WriteFile(filepath.Join(e.Cd, "invalid.txt"), []byte("aaaaaa"), 0600)
+			require.NoError(t, err)
+			urlSecret := "EW32D2CFTAIRTEAWTRQZZXAITVA4U6K4"
+			url := fmt.Sprintf("otpauth://totp/example.com:foo@example.com?algorithm=SHA1&digits=6&issuer=example.com&period=30&secret=%s", urlSecret)
+			err = os.WriteFile(filepath.Join(e.Cd, "urlsecret.txt"), []byte(urlSecret), 0600)
+			require.NoError(t, err)
+			key, err := otp.NewKeyFromURL(url)
+			require.NoError(t, err)
+			urlCode, err := totp.GenerateCode(key.Secret(), time.Now())
+			require.NoError(t, err)
+			err = os.WriteFile(filepath.Join(e.Cd, "urlcode.txt"), []byte(urlCode), 0600)
+			require.NoError(t, err)
+			return nil
+		},
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"check_otp": checkOTP,
+		},
+	})
+}
+
+func checkOTP(ts *testscript.TestScript, neg bool, args []string) {
+	out := strings.TrimSpace(ts.ReadFile(args[0]))
+
+	length, err := strconv.Atoi(args[1])
+	ts.Check(err)
+
+	if out == "" {
+		ts.Fatalf("expected OTP not be empty")
+	}
+
+	if length != -1 {
+		if len(out) != length {
+			ts.Fatalf("expected OTP to be %d characters long; got %d", length, len(out))
+		}
+	}
+
+	if strings.HasPrefix(out, "otpauth://") {
+		key, err := otp.NewKeyFromURL(out)
+		ts.Check(err)
+
+		switch {
+		case key.Type() != "totp":
+			ts.Fatalf("expected OTP to be type totp; got %s", key.Type())
+		case key.Issuer() != "example.com":
+			ts.Fatalf("expected issuer to be example.com; got %s", key.Issuer())
+		case key.AccountName() != "foo@example.com":
+			ts.Fatalf("expected account name to be foo@example.com; got %s", key.AccountName())
+		case len(key.Secret()) != 32:
+			ts.Fatalf("expected secret to be 32 bytes; got %d", len(key.Secret()))
+		}
+	}
+}
+
 func TestCryptoHelp(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Files: []string{"testdata/crypto/help.txtar"},
 	})
-}
-
-func keyLength(jwk *jose.JSONWebKey) (int, error) {
-	switch key := jwk.Key.(type) {
-	case []byte:
-		return len(key) * 8, nil
-	case *rsa.PrivateKey:
-		return key.N.BitLen(), nil
-	case *rsa.PublicKey:
-		return key.N.BitLen(), nil
-	case *ecdsa.PrivateKey:
-		return key.Params().BitSize, nil
-	case *ecdsa.PublicKey:
-		return key.Params().BitSize, nil
-	default:
-		return 0, fmt.Errorf("unsupported key type: %T", key)
-	}
-}
-
-func keyCurve(jwk *jose.JSONWebKey) (elliptic.Curve, error) {
-	switch key := jwk.Key.(type) {
-	case *ecdsa.PrivateKey:
-		return key.Curve, nil
-	case *ecdsa.PublicKey:
-		return key.Curve, nil
-	default:
-		return nil, fmt.Errorf("unsupported key type: %T", key)
-	}
 }
 
 // checkKeyPair checks that the public/private key pair is valid. It performs
@@ -181,5 +219,33 @@ func checkKeyPair(ts *testscript.TestScript, neg bool, args []string) {
 		}
 	default:
 		ts.Fatalf("unknown curve %q", crv)
+	}
+}
+
+func keyLength(jwk *jose.JSONWebKey) (int, error) {
+	switch key := jwk.Key.(type) {
+	case []byte:
+		return len(key) * 8, nil
+	case *rsa.PrivateKey:
+		return key.N.BitLen(), nil
+	case *rsa.PublicKey:
+		return key.N.BitLen(), nil
+	case *ecdsa.PrivateKey:
+		return key.Params().BitSize, nil
+	case *ecdsa.PublicKey:
+		return key.Params().BitSize, nil
+	default:
+		return 0, fmt.Errorf("unsupported key type: %T", key)
+	}
+}
+
+func keyCurve(jwk *jose.JSONWebKey) (elliptic.Curve, error) {
+	switch key := jwk.Key.(type) {
+	case *ecdsa.PrivateKey:
+		return key.Curve, nil
+	case *ecdsa.PublicKey:
+		return key.Curve, nil
+	default:
+		return nil, fmt.Errorf("unsupported key type: %T", key)
 	}
 }
