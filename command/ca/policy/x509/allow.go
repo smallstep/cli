450bd22a1c80458ffcf0cf67db94f03a661d2c3b
Carl Tashian <carl@smallstep.com>
2025-07-21 10:19:09 -0700

    Add required <scope> param to help text for step ca policy

--- command/ca/policy/x509/allow.go
+++ command/ca/policy/x509/allow.go
@@ -15,8 +15,8 @@ func allowCommand(ctx context.Context) cli.Command {
 	return cli.Command{
 		Name:        "allow",
 		Usage:       "manage allowed names for X.509 certificate issuance policies",
-		UsageText:   "**step ca policy x509 allow** <subcommand> [arguments] [global-flags] [subcommand-flags]",
-		Description: `**step ca policy x509 allow** command group provides facilities for managing X.509 names to be allowed.`,
+		UsageText:   "**step ca policy <scope> x509 allow** <subcommand> [arguments] [global-flags] [subcommand-flags]",
+		Description: `**step ca policy <scope> x509 allow** command group provides facilities for managing X.509 names to be allowed.`,
 		Subcommands: cli.Commands{
 			actions.CommonNamesCommand(ctx),
 			actions.DNSCommand(ctx),
